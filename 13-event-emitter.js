82cebe30b04ff015f026c08cd23615b2e448de59
Signal Fish <signalfish38861721@gmail.com>
2021-10-17 08:26:50 -0400

    request emitter, stream

--- 13-event-emitter.js
+++ 13-event-emitter.js
@@ -1,21 +1,24 @@
-// get back the class
-// if want custom extend from class
-// otherwise just for emitting and handling events create instance
 const EventEmitter = require("events");
-
 const customEmitter = new EventEmitter();
 
-// on and emit methods
-// keep track of the order
-// additional arguments
-// built-in modules utilize it
+// Assign the event handler to the response event
 
-customEmitter.on("response", (name, id) => {
-  console.log(`data recieved user ${name} with id:${id}`);
-});
+customEmitter.on(
+  "response",
+  // event handler
+  (name, id) => {
+    console.log(`data recieved user ${name} with id:${id}`);
+  }
+);
 
-customEmitter.on("response", () => {
-  console.log("some other logic here");
-});
+// Assign the event handler to the response event
+customEmitter.on(
+  "response",
+  // event handler
+  () => {
+    console.log("some other logic here");
+  }
+);
 
+// Fire the response event
 customEmitter.emit("response", "Signal Fish", 999);
---
+++ 14-request-event.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const port = 5000
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
+
+// Using Event Emitter API
+const server = http.createServer();
+
+// emits request event
+// subcribe to it / listen for it / respond to it
+server.on("request", (req, res) => {
+  res.end("Welcome");
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
